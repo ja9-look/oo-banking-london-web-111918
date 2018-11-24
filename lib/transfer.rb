0f95e436eff0a94321d7826e2db2aac137fbad85
Janine Luk <janineluk95@gmail.com>
2018-11-24 22:20:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,12 @@
 class Transfer
   # your code here
   
+  def initialize(sender, receiver)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @trasfer_amount = 
     
+  end
     
 end
