36ce6c5b7c9e83b06695e350f853ba8f7b11bac2
Janine Luk <janineluk95@gmail.com>
2018-11-24 22:52:45 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,9 @@ class Transfer
       @sender.balance -= amount
       @receiver.balance += amount
       @status = "complete"
+    else
+      @status = "rejected"
+      return 
     end
   end
   
