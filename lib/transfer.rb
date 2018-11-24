cf4b330c434abe86ff948cdf331b1f5ad705b8fc
Janine Luk <janineluk95@gmail.com>
2018-11-24 22:52:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,11 @@ class Transfer
   end
   
   def execute_transaction
-    if @status = "pending" && @sender.balance > @amount
-      
+    if @status == "pending" && @sender.balance > @amount
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
+    end
   end
   
 end
